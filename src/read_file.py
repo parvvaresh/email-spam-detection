854a4a8db79615abfaa43945eaaab6d63baf7cf4
Alireza Parvaresh <89921883+parvvaresh@users.noreply.github.com>
2023-12-27 21:55:11 +0330

    Update read_file.py

--- src/read_file.py
+++ src/read_file.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import os
+from data.get_path import get_ham, get_spam
 
-def _create_dataset(path, label):
-  files = [file for file in os.listdir(path)]
+def _create_dataset(paths, label):
   data = list()
-  for file in files:
-    with open(path +"/" + file, "r" , encoding='utf-8') as f:
+  for path in paths:
+    with open(path, encoding='utf-8') as f:
       temp = f.read()
       data.append(temp)
 
@@ -16,10 +16,11 @@ def _create_dataset(path, label):
 
 def create_dataset():
 
-  train = pd.concat([_create_dataset('/home/alireza/Desktop/src/emails/hamtraining/', "ham"),  _create_dataset('/home/alireza/Desktop/src/emails/spamtraining/', "spam")] , axis=0)
-  test = pd.concat([_create_dataset('/home/alireza/Desktop/src/emails/hamtesting/', "ham"), _create_dataset('/home/alireza/Desktop/src/emails/spamtesting/', "spam")], axis=0)
+  spam = _create_dataset(get_spam(), "spam")
+  ham = _create_dataset(get_ham(), "ham")
 
-  train["target"] = train["label"].replace(["spam", "ham"], [1, 0])
-  test["target"] = test["label"].replace(["spam", "ham"], [1, 0])
-  df = pd.concat([test, train ], axis=0)
+
+  df = pd.concat([spam, ham ], axis=0)
+  df["target"] = df["label"].replace(["spam", "ham"], [1, 0])
+
   return df

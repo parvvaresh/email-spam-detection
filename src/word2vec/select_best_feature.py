88eab1992e8732f35c3ae5f2f7f69ee08a6c5476
Alireza Parvaresh <89921883+parvvaresh@users.noreply.github.com>
2023-12-27 21:52:47 +0330

    Update select_best_feature.py

--- src/word2vec/select_best_feature.py
+++ src/word2vec/select_best_feature.py
@@ -1,6 +1,9 @@
 from sklearn.feature_selection import SelectKBest, chi2
 
-def selectKBest(x, y, k = 250):
-	transformer = SelectKBest(chi2, k = 250).fit(x, y)
-	x = transformer.transform(x)
-	return x
+def select_KBest(x_train, x_test, y_train, y_test, k = 250):
+    
+	transformer = SelectKBest(chi2, k=k).fit(x_train, y_train)
+	x_train = transformer.transform(x_train)
+	x_test = transformer.transform(x_test)
+
+	return x_train, x_test, y_train, y_test
